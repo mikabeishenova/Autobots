6c8eb8d463797aca32ef2f62fd3f10e10777261e
Mika <mikabeishenova@gmail.com>
2025-07-24 09:39:52 +0600

    Verion 1.0.2

--- src/main/java/REsetDemo.java
+++ src/main/java/REsetDemo.java
@@ -2,5 +2,6 @@ public class REsetDemo {
     public static void main(String[] arg){
         System.out.println("Version 1.0.0");
         System.out.println("Version 1.0.1");
+        System.out.println("Version 1.0.2");
     }
 }
