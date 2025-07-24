0bd90ba39151449727d3e68468254fbf4746c42e
Mika <mikabeishenova@gmail.com>
2025-07-24 10:13:04 +0600

    version 1.0.4

--- src/main/java/REsetDemo.java
+++ src/main/java/REsetDemo.java
@@ -1,9 +1,9 @@
-public class REsetDemo {
+public class ResetDemo {
     public static void main(String[] arg){
         System.out.println("Version 1.0.0");
         System.out.println("Version 1.0.1");
         System.out.println("Version 1.0.2");
         System.out.println("Version 1.0.3");
-        System.out.println("Version 1.0.4 --> Hello World");
+        System.out.println("Version 1.0.4 --> Mixed");
     }
 }
